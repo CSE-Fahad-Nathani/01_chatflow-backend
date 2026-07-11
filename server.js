76f25cc2bff75f7c1fe13f3d67afd2921bbfb09b
Fahad Nathani <fahadnathani1109@gmail.com>
2026-07-11 12:52:06 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,113 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+const messages = [];
+
+
+app.get("/session", (req, res) => {
+  const { websiteId, sessionId } = req.query;
+
+  console.log("Website ID:", websiteId);
+
+  res.json({
+    success: true,
+    websiteId,
+    sessionId: sessionId || `sess_${Date.now()}`,
+    visitorId: `visitor_${Math.floor(Math.random() * 100000)}`,
+    config: {
+      title: "Infomanav Support",
+      primaryColor: "#2563eb",
+    },
+  });
+});
+
+
+/*
+  GET /session
+*/
+app.get("/sessions", (req, res) => {
+  const sessions = [...new Set(messages.map((m) => m.sessionId))];
+
+  res.json({
+    success: true,
+    sessions,
+  });
+});
+
+app.post("/message", (req, res) => {
+  const { sessionId, message } = req.body;
+
+  const newMessage = {
+    id: Date.now(),
+    sessionId,
+    message,
+    sender: "visitor",
+    createdAt: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  console.log(messages);
+
+  res.json({
+    success: true,
+    message: newMessage,
+  });
+});
+
+
+app.get("/messages", (req, res) => {
+  const { sessionId } = req.query;
+
+  const sessionMessages = messages.filter(
+    (message) => message.sessionId === sessionId
+  );
+
+  res.json({
+    success: true,
+    messages: sessionMessages,
+  });
+});
+
+
+
+app.post("/reply", (req, res) => {
+  const { sessionId, message } = req.body;
+
+  const newMessage = {
+    id: Date.now(),
+    sessionId,
+    message,
+    sender: "agent",
+    createdAt: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  res.json({
+    success: true,
+    message: newMessage,
+  });
+});
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
